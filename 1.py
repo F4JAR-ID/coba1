5ee9b8556385122571e38e3e685e1987a5dc7177
Fajar Septianto <49784833+F4JAR-ID@users.noreply.github.com>
2019-06-10 18:31:31 +0700

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,40 @@
+#!usr
+# -*- coding: UTF-8 -*-
+# Mod by: iqbalz noobs
+# team: life of programmer
+# Fajar Id Gans :v
+
+
+import os
+import sys
+import time
+import random
+import cookielib
+import mechanize
+
+wd = "\033[90;1m" # dark
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+C = "\033[36;1m"    # Cyan
+
+def runntxt(s):
+        for noobs in s + '\n':
+                sys.stdout.write(noobs)
+                sys.stdout.flush()
+                time.sleep(10. / 2100)
+
+
+def banner():
+    os.system('clear')
+    print " "
+    runntxt
